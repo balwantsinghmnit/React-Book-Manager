c214184c6fe106dea6eeea323be9137117f5ba49
balwantsinghmnit <balwantsinghmnit@gmail.com>
2020-06-27 15:43:38 +0530

    react book manager

--- src/Header.js
+++ src/Header.js
@@ -7,17 +7,17 @@ function Header(props)
 		<header>
 			<nav className="navbar navbar-expand-lg navbar-dark redback">
 			  <div className="container-fluid">
-			    <Link className="navbar-brand" to="/">Book Manager</Link>
+			    <a className="navbar-brand" href="/">Book Manager</a>
 			    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
 			      <span className="navbar-toggler-icon"></span>
 			    </button>
 			    <div className="collapse navbar-collapse ml-md-5 ml-lg-5" id="navbarColor02">
 			      <ul className="navbar-nav mr-auto mb-2 mb-lg-0  ml-md-5 ml-lg-5">
 			        <li className="nav-item active  ml-md-5 ml-lg-5">
-			          <Link className="nav-link btn" aria-current="page" to="/">Home</Link>
+			          <a className="nav-link btn" aria-current="page" href="/">Home</a>
 			        </li>
 			        <li className="nav-item  ml-md-5 ml-lg-5">
-			          <Link className="nav-link btn" to="/about">About</Link>
+			          <a className="nav-link btn" href="/about">About</a>
 			        </li>
 			      </ul>
 			    </div>
